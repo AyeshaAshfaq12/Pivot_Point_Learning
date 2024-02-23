76c1ab2c31affef640c5b02150cd4cbf0285d6f4
AyeshaAshfaq12 <ayeshaashfaq925@gmail.com>
2024-02-23 22:59:03 +0500

    Frontend Complete

--- learningapp/urls.py
+++ learningapp/urls.py
@@ -3,7 +3,13 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('personalized_learning/', views.personalized_learning, name="personalized_learning"),
+    path('resources/', views.resources, name="resources"),
+    path('contact/', views.contact, name="contact"),
+    path('survey1/', views.survey1, name="survey1"),
+    path('survey2/', views.survey2, name="survey2"),
 ]
 
 
--- learningapp/views.py
+++ learningapp/views.py
@@ -2,9 +2,37 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse, JsonResponse
 
 
-
 # Create your views here.
 
 
-def index():
-    pass
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def personalized_learning(request):
+    return render(request, 'personalized_learning.html')
+
+
+def resources(request):
+    return render(request, 'resources.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def survey1(request):
+    return render(request, 'survey1.html')
+
+
+def survey2(request):
+    return render(request, 'survey2.html')
+
+
+
+
+
